f52f088c01a5240f9e001cfa9914b010570a7561
Slade Lim <sladelim@gmail.com>
2024-01-21 16:15:25 -0700

    added footer icons and started footer

--- src/containers/footer/Footer.tsx
+++ src/containers/footer/Footer.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './footer.css'
+import logo from '../../assets/logo.png'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='sladelim__footer section__padding'>
+      <div className='sladelim__footer-links'>
+        <div className='sladelim__footer-links_logo'>
+          <img src={logo} alt="logo" />
+        </div>
+        <div className='sladelim__footer-links-div'>
+          <h4>Follow me</h4>
+        </div>
+      </div>
+    </div>
   )
 }
 
